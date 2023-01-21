c7d92ba31331528d40633a8b7592bb05eb3bde09
faissaloux <fwahabali@gmail.com>
2023-01-21 17:51:19 +0100

    lint github workflow

--- geranslator/config/config.py
+++ geranslator/config/config.py
@@ -4,9 +4,9 @@ from typing import Optional
 from ..exceptions.ConfigKeyNotFound import ConfigKeyNotFound
 
 class Config:
-    __dir__ = os.path.dirname(os.path.realpath(__file__))
+    dir: str = os.path.dirname(os.path.realpath(__file__))
     config_relative_path: str = os.path.join('..', 'config.yml')
-    config_path: str = os.path.join(__dir__, config_relative_path)
+    config_path: str = os.path.join(dir, config_relative_path)
 
     def get(self, key: Optional[str] = None):
         if os.path.exists(self.config_path):
