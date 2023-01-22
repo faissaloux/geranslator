0600be2e27b5064d7e477b68d4a6804610410fb5
faissaloux <fwahabali@gmail.com>
2023-01-22 16:56:00 +0100

    fix mypy complain

--- geranslator/provider/providers/google.py
+++ geranslator/provider/providers/google.py
@@ -3,5 +3,6 @@ from typing import List
 class Google:
     url: str = 'https://translate.google.com'
 
+    @staticmethod
     def translate(lang_from_file: str, lang_to_files: List[str]):
         pass
