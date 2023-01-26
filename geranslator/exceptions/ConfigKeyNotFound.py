62cfb179e20976d2a80c7c290828041f0c925ba3
faissaloux <fwahabali@gmail.com>
2023-01-26 01:53:41 +0100

    refactor exceptions

--- geranslator/exceptions/ConfigKeyNotFound.py
+++ geranslator/exceptions/ConfigKeyNotFound.py
@@ -1,9 +1,10 @@
-import termspark.termspark
+from termspark import TermSpark
 
 class ConfigKeyNotFound(Exception):
     def __init__(self, key: str):
         self.key = key
 
-    def __str__(self):
-        message = termspark.TermSpark().spark_left([f" {self.key} ", 'white', 'red'], [f" not found in config file ", 'red'])
+    def __str__(self) -> str:
+        message = TermSpark().spark_left([f" {self.key} ", 'white', 'red'], [f" not found in config file ", 'red'])
+
         return str(message)
