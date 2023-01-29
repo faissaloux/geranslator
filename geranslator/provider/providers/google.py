357439e6b986e256ab3a5d5b584c38ac6af13c0b
faissaloux <fwahabali@gmail.com>
2023-01-29 20:48:19 +0100

    AbstractProvider

--- geranslator/provider/providers/google.py
+++ geranslator/provider/providers/google.py
@@ -1,20 +1,18 @@
 import time
 
 from typing import List
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
-from webdriver_manager.chrome import ChromeDriverManager
 from termspark import TermSpark
+from .abstractProvider import AbstractProvider
 from ...languages.languages import Languages
 from ...exceptions.UnsupportedLanguage import UnsupportedLanguage
 
-class Google:
+class Google(AbstractProvider):
     url: str = 'https://translate.google.com'
     words_to_translate: list = []
     translation: dict
@@ -32,16 +30,16 @@ class Google:
             for lang in lang_to_files:
                 self.translation[lang] = {}
 
-                self.__choose_languages(origin_lang, lang)
+                self.choose_languages(origin_lang, lang)
 
-                self.__translate_for(lang)
+                self.translate_for(lang)
 
             return self.translation
         except WebDriverException as e:
             TermSpark().spark_left([' Please check your network and try again ', 'white', 'red']).spark()
             return {}
 
-    def __translate_for(self, lang):
+    def translate_for(self, lang: str):
         for word_to_translate in self.words_to_translate:
             source_text = WebDriverWait(self.driver, 15).until(
                 expected_conditions.presence_of_element_located((
@@ -62,14 +60,14 @@ class Google:
             self.translation[lang][word_to_translate] = translated_element.text
             source_text.clear()
 
-    def __choose_languages(self, lang_from, lang_to):
+    def choose_languages(self, lang_from: str, lang_to: str):
         more_source_languages_btn = WebDriverWait(self.driver, 15).until(
             expected_conditions.presence_of_element_located((
                 By.XPATH, "//button[@aria-label='More source languages']"
             ))
         )
         more_source_languages_btn.click()
-        self.__search_language(Languages().get(lang_from))
+        self.search_language(Languages().get(lang_from))
 
         time.sleep(2)
 
@@ -79,9 +77,9 @@ class Google:
             ))
         )
         more_target_languages_btn.click()
-        self.__search_language(Languages().get(lang_to))
+        self.search_language(Languages().get(lang_to))
 
-    def __search_language(self, language):
+    def search_language(self, language: str):
         WebDriverWait(self.driver, 15).until(
             expected_conditions.presence_of_element_located((
                 By.XPATH, "//input[@aria-label='Search languages']"
@@ -103,13 +101,3 @@ class Google:
                     ActionChains(self.driver).send_keys(Keys.DOWN, Keys.RETURN).perform()
 
                 time.sleep(2)
-
-    def open_browser(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument("headless")
-
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        self.driver.maximize_window()
-
-    def close_browser(self):
-        self.driver.quit()
