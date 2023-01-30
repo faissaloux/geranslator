37263f31ef277f63b5a6cef17f908ed693044575
faissaloux <fwahabali@gmail.com>
2023-01-30 11:51:08 +0100

    support deepl provider

--- geranslator/provider/providers/deepl.py
+++ geranslator/provider/providers/deepl.py
@@ -9,14 +9,14 @@ from .abstractProvider import AbstractProvider
 from ...languages.languages import Languages
 from ...exceptions.UnsupportedLanguage import UnsupportedLanguage
 
-class Google(AbstractProvider):
-    url: str = 'https://translate.google.com'
+class Deepl(AbstractProvider):
+    url: str = 'https://www.deepl.com/translator'
 
     def translate_for(self, lang: str):
         for word_to_translate in self.words_to_translate:
             source_text = WebDriverWait(self.driver, 15).until(
                 expected_conditions.presence_of_element_located((
-                    By.XPATH, "//textarea[@aria-label='Source text']"
+                    By.XPATH, "//textarea[@dl-test='translator-source-input']"
                 ))
             )
             ActionChains(self.driver).move_to_element(source_text).click().send_keys(word_to_translate).perform()
@@ -25,18 +25,21 @@ class Google(AbstractProvider):
 
             WebDriverWait(self.driver, 15).until(
                 expected_conditions.presence_of_element_located((
-                    By.XPATH, "//button[@aria-label='Copy translation']"
+                    By.XPATH, "//textarea[@dl-test='translator-target-input']"
                 ))
             )
 
-            translated_element = self.driver.find_element(by=By.XPATH, value="//span[@class='HwtZe']")
-            self.translation[lang][word_to_translate] = translated_element.text
+            translated_element = self.driver.find_element(by=By.XPATH, value="//textarea[@dl-test='translator-target-input']")
+            self.translation[lang][word_to_translate] = translated_element.get_attribute('value')
+
             source_text.clear()
 
     def choose_languages(self, lang_from: str, lang_to: str):
+        self.__remove_advertisement()
+
         more_source_languages_btn = WebDriverWait(self.driver, 15).until(
             expected_conditions.presence_of_element_located((
-                By.XPATH, "//button[@aria-label='More source languages']"
+                By.XPATH, "//button[@dl-test='translator-source-lang-btn']"
             ))
         )
         more_source_languages_btn.click()
@@ -46,7 +49,7 @@ class Google(AbstractProvider):
 
         more_target_languages_btn = WebDriverWait(self.driver, 15).until(
             expected_conditions.presence_of_element_located((
-                By.XPATH, "//button[@aria-label='More target languages']"
+                By.XPATH, "//button[@dl-test='translator-target-lang-btn']"
             ))
         )
         more_target_languages_btn.click()
@@ -55,22 +58,33 @@ class Google(AbstractProvider):
     def search_language(self, language: str):
         WebDriverWait(self.driver, 15).until(
             expected_conditions.presence_of_element_located((
-                By.XPATH, "//input[@aria-label='Search languages']"
+                By.XPATH, "//input[@placeholder='Search languages']"
             ))
         )
 
         time.sleep(2)
-
-        search_language_elements = self.driver.find_elements(by=By.XPATH, value="//input[@aria-label='Search languages']")
+        search_language_elements = self.driver.find_elements(by=By.XPATH, value="//input[@placeholder='Search languages']")
 
         for search_language_element in search_language_elements:
             if search_language_element.size['width']:
                 ActionChains(self.driver).move_to_element(search_language_element).click().send_keys(language).perform()
-                unexisted_language = self.driver.find_element(by=By.XPATH, value="//div[@class='G3Fn7c'][contains(.,'No results')]").is_displayed()
+                time.sleep(2)
+                unexisted_language = self.driver.find_elements(by=By.XPATH, value="//div[@class='lmt__sides_wrapper'][contains(., 'No results')]")
 
-                if unexisted_language:
+                if len(unexisted_language):
                     raise UnsupportedLanguage(language)
                 else:
-                    ActionChains(self.driver).send_keys(Keys.DOWN, Keys.RETURN).perform()
+                    ActionChains(self.driver).send_keys(Keys.RETURN).perform()
 
                 time.sleep(2)
+
+    def __remove_advertisement(self):
+        try:
+            close_advertisement_popup_btn = WebDriverWait(self.driver, 15).until(
+                expected_conditions.presence_of_element_located((
+                    By.XPATH, "//div[@data-testid='write-advertisement-popup']/button[@aria-label='Close']"
+                ))
+            )
+            close_advertisement_popup_btn.click()
+        except:
+            pass
--- tests/provider_test.py
+++ tests/provider_test.py
@@ -1,9 +1,14 @@
 import pytest
+import random
 
 from geranslator.provider.provider import Provider
 from geranslator.exceptions.ProviderNotFound import ProviderNotFound
 
 class TestProvider:
+    supported_providers: list = [
+        'google',
+        'deepl',
+    ]
 
     def test_set_provider(self):
         provider = Provider('google')
@@ -15,20 +20,16 @@ class TestProvider:
 
         assert provider.provider == 'google'
 
-    def test_make_sure_existing_provider_exist(self):
-        provider = Provider('google')
-
-        assert provider.make_sure_provider_exists() == True
-
-    def test_make_sure_unexisting_provider_unexist(self):
-        with pytest.raises(ProviderNotFound):
-            Provider('unexisted').make_sure_provider_exists()
+    def test_supported_providers(self):
+        for provider in self.supported_providers:
+            assert Provider(provider).provider == provider
 
     def test_cant_translate_using_unexisted_provider(self):
         with pytest.raises(ProviderNotFound):
             Provider('unexisted').translate(['Hello'], 'en', ['ar', 'fr'])
 
     def test_translation(self):
-        translation = Provider('google').translate(['Hello'], 'en', ['es', 'pt'])
+        provider = random.choice(self.supported_providers)
+        translation = Provider(provider).translate(['Hello'], 'en', ['es', 'pt'])
 
         assert translation == {'es': {'Hello': 'Hola'}, 'pt': {'Hello': 'Olá'}}
