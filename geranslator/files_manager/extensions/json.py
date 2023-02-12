41e5588683e2f47cfaaac435555b20576d311e86
faissaloux <fwahabali@gmail.com>
2023-02-12 01:16:59 +0100

    revert last commit

--- geranslator/files_manager/extensions/json.py
+++ geranslator/files_manager/extensions/json.py
@@ -13,17 +13,14 @@ class Json(AbstractExtension):
     def get(self, file: str) -> dict:
         data: dict = {}
 
-        if self.hidden:
-            for hidden in self.hidden:
-                for key, value in json.load(open(file, "r")).items():
-                    for word in value.split():
-                        if bool(re.search(hidden, word)):
-                            data[key] = {}
-                            data[key][word] = value.split(word)
-                            break
-                        else:
-                            data[key] = value
-        else:
-            data = json.load(open(file, "r"))
+        for hidden in self.hidden:
+            for key, value in json.load(open(file, "r")).items():
+                for word in value.split():
+                    if bool(re.search(hidden, word)):
+                        data[key] = {}
+                        data[key][word] = value.split(word)
+                        break
+                    else:
+                        data[key] = value
 
         return data
